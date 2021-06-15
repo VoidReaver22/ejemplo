d86732a21444e4a6481c70fcace6e2b5db9757de
Maty Russo <matias_russo@outlook.es>
2021-06-15 10:15:10 -0300

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Holiwi UWU</h1>
+    <p>Aca estoy haciendo cositas</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
